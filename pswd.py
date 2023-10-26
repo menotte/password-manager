da0095925ed9d3e2bcfc56d2f15a37610caf0993
menotte <139005962+menotte@users.noreply.github.com>
2023-10-26 12:38:09 +0200

    Update pswd.py

--- pswd.py
+++ pswd.py
@@ -3,7 +3,7 @@ import string
 import os
 
 def generate_random_password(length=16):
-    characters = string.ascii_letters + string.digits
+    characters = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(random.choice(characters) for _ in range(length))
     return password
 
